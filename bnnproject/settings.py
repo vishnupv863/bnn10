8ee58d202058806f5817aeff6cb804621cc3d7a2
vishnupv863 <vishnupv863@gmail.com>
2024-06-20 22:49:27 +0530

    2nd

--- bnnproject/settings.py
+++ bnnproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+dildp(ljk%x*8*(kl+@m23-@b(cjo&op0wzal7ihq5c0)$)qh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
